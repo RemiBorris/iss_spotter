8ae41e6616ccdd87269c60b846fb61541efcdd59
remiborris <remiborris@gmail.com>
2024-06-14 17:14:49 -0300

    create function fetchCoordsByIP to return latitude nad longitude in object form from a given IP address and import into index

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 // index.js
 const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+/** // fetchMyIP test code below ----------
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
 
-//   console.log('It worked! Returned IP:' , ip);
-// });
+  console.log('It worked! Returned IP:' , ip);
+});
+*/
+
+/** //  fetchCoordsByIP test code below ----------
+fetchCoordsByIP('174.116.166.225', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
+*/
--- iss.js
+++ iss.js
@@ -1,5 +1,6 @@
 const needle = require('needle');
 const ipv4Url = "https://api.ipify.org/?format=json";
+const locationUrl = "http://ipwho.is/";
 
 const fetchMyIP = function(callback) {
   needle.get(ipv4Url, (error, response, body) => {
@@ -16,4 +17,22 @@ const fetchMyIP = function(callback) {
   });
 };
 
+const fetchCoordsByIP = function(ip, callback) {
+  const location = {};
+  needle.get(locationUrl + ip,(error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (!body.success) {
+      const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${ip}`;
+      callback(Error(message), null)
+      return;
+    }
+    location.latitude = body.latitude;
+    location.longitude = body.longitude;
+    callback(null, location);
+  });
+};
+
 module.exports = {fetchMyIP};
+module.exports = {fetchCoordsByIP};

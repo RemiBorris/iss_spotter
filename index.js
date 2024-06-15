4a86213e71e38ecb81e45cbea744fa52328b0573
remiborris <remiborris@gmail.com>
2024-06-15 09:17:46 -0300

    create function to get the passes from the ISS API with an object latitude and longitude

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // index.js
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
-/** // fetchMyIP test code below ----------
+// fetchMyIP test code below ----------
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -11,9 +10,9 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 });
-*/
 
-/** //  fetchCoordsByIP test code below ----------
+
+//  fetchCoordsByIP test code below ----------
 fetchCoordsByIP('174.116.166.225', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
@@ -22,4 +21,14 @@ fetchCoordsByIP('174.116.166.225', (error, coordinates) => {
 
   console.log('It worked! Returned coordinates:' , coordinates);
 });
-*/
+
+
+
+fetchISSFlyOverTimes({ latitude: 46.0878165, longitude: -64.7782313 }, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned pass times:' , passTimes);
+});
--- iss.js
+++ iss.js
@@ -25,7 +25,7 @@ const fetchCoordsByIP = function(ip, callback) {
     }
     if (!body.success) {
       const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${ip}`;
-      callback(Error(message), null)
+      callback(Error(message), null);
       return;
     }
     location.latitude = body.latitude;
@@ -34,5 +34,19 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
-module.exports = {fetchMyIP};
-module.exports = {fetchCoordsByIP};
+
+const fetchISSFlyOverTimes = function(coords, callback) {
+  needle.get(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) return callback(error,null);
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching ISS pass times: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const passes = response.body;
+    callback(null, passes);
+  });
+};
+
+module.exports = {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes};
